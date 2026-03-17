84ad42f731c04b0d8594b1eeeb7ce249a3bd828c
Leonardo-de-Aguiar <leonardo_n_aguiar@estudante.sesisenai.org.br>
2026-03-17 08:39:20 -0300

    add - primeiro commit e fiz o exercicio 1

---
+++ exercicio_01.js
@@ -0,0 +1,25 @@
+ console.log("Olá, Mundo!");
+
+ let nome = "Leonardo";
+ let idade = 17;
+ let peso = 57;
+ let sabe = true;
+
+ console.log("Meu nome é:" + nome);
+
+ function somarNumeros(){
+    let soma = 0;
+    let continuar = true;
+    while (continuar){
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar):"));
+        if (!isNaN(numero)){
+            soma += numero;
+        } else {
+            alert("Por favor, digite um número válido.");
+        }
+        continuar = confirm("Deseja adicionar mais números?");
+    }
+    alert("A soma dos números é: " + soma);
+ }
+
+ somarNumeros();
