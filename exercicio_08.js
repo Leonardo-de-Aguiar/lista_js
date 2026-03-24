d9594796541a52eea440f0dd42a839c92d612a5b
Leonardo-de-Aguiar <leonardo_n_aguiar@estudante.sesisenai.org.br>
2026-03-24 08:15:32 -0300

    add - exercicio 8

---
+++ exercicio_08.js
@@ -0,0 +1,11 @@
+function operacaoVariaveis(){
+
+    let a = 3;
+    let b = 2;
+
+    alert("os valores são A: " + a + " e B: " + b );
+    alert("A diferença é: " + (a - b));
+    alert("O dobro da primeira somado ao triplo da segunda é: " + (2*a + 3*b));
+    alert("A multiplicação das duas variáveis é: " + (a * b));
+
+}
